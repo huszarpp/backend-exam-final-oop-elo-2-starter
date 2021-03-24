251c9bea3c5e71a3896e8206bb937290b6f80b85
huszarpp <huszarpp@gmail.com>
2021-03-24 15:40:09 +0100

    Implement Farm

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,40 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Farm {
 
+    private final List<Animal> animals;
+
+    public Farm(List<Animal> animalList) {
+        this.animals = animalList;
+    }
+
+    public List<Animal> getAnimals() {
+        return new ArrayList<>(animals);
+    }
+
+    public void feedAnimals() {
+        animals.forEach(Animal::feed);
+    }
+
+    public void butcher(Butcher butcher) {
+        animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return animals.size() == 0;
+    }
+
+    public List<String> getStatus() {
+        return animals
+                .stream()
+                .map(a -> "There is a " + a.getSize() + " sized "
+                        + a.getClass().getSimpleName().toLowerCase() + " in the farm.")
+                .collect(Collectors.toList());
+    }
 }
