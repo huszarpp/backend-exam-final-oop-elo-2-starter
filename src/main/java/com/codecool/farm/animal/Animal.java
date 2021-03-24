a80551da9dd4b3324ac3d6df8b1910d044ca0161
huszarpp <huszarpp@gmail.com>
2021-03-24 15:21:37 +0100

    Implement Animal, Cattle, Pig

--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,15 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    private int size = 0;
+
+    public abstract void feed();
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
--- src/main/java/com/codecool/farm/animal/Pig.java
+++ src/main/java/com/codecool/farm/animal/Pig.java
@@ -2,4 +2,8 @@ package com.codecool.farm.animal;
 
 public class Pig extends Animal {
 
+    @Override
+    public void feed() {
+        setSize(getSize() + 1);
+    }
 }
